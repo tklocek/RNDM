34e9e9b6789f8c78d81b67e8f190f2186c6c27bb
Tomasz Klocek <tomek@klocek.info>
2020-01-27 22:28:53 +0100

    first commit

---
+++ RNDM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RNDM
+//
+//  Created by Tomek Klocek on 2020-01-27.
+//  Copyright © 2020 Tomek Klocek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
